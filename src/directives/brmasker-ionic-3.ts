12ef0c2d63bc1abc923288715cf6c9db777389b1
przyczynski <thiago89>
2018-03-05 17:25:32 -0300

    Ajuste na máscara monetária (permitindo milhares e milhões)

--- src/directives/brmasker-ionic-3.ts
+++ src/directives/brmasker-ionic-3.ts
@@ -134,8 +134,18 @@ export class BrMaskerIonic3 implements OnInit, ControlValueAccessor {
   }
 
   private moneyMask(v: any): string {
-    let tmp = v;
-    tmp = tmp.replace(/([0-9]{2})$/g, ',$1');
+    var tmp = v+'';
+
+    tmp = tmp.replace(/([0-9]{2})$/g, ",$1");
+    if( tmp.length > 6)
+      tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+
+    if( tmp.length > 9)
+      tmp = tmp.replace(/([0-9]{3}).([0-9]{3}),([0-9]{2}$)/g,".$1.$2,$3");
+
+    if( tmp.length > 12)
+      tmp = tmp.replace(/([0-9]{3}).([0-9]{3}).([0-9]{3}),([0-9]{2}$)/g,".$1.$2.$3,$4");
+
     return tmp;
   }
 
